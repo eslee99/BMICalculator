923e8568db440c022faa528241e3a98a98aa0507
eslee99 <leeshijing_99@hotmail.co.uk>
2019-11-04 19:37:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageCategory.setImageResource(R.drawable.defaultt)
+        btnCalculate.setOnClickListener(){
+            calculateBMI()
+        }
+        btnReset.setOnClickListener(){
+            reset()
+        }
+    }
+    private fun calculateBMI() {
+        val height=editHeight.text.toString().toFloat()
+        val weight=editWeight.text.toString().toFloat()
+        val bmi=weight /(height*height)
+
+        textBmi.setText(getString(R.string.bmi)+": "+bmi)
+
+        if(bmi<18.5) {
+            textCategory.setText(getString(R.string.category) + ": " + getString(R.string.underWeight))
+            imageCategory.setImageResource(R.drawable.under)
+        }
+        else if(bmi>=18.5&&bmi<=24.9){
+            textCategory.setText(getString(R.string.category)+": "+getString(R.string.normal))
+            imageCategory.setImageResource(R.drawable.normal)
+        }
+        else {
+            textCategory.setText(getString(R.string.category) + ": " + getString(R.string.overWeight))
+            imageCategory.setImageResource(R.drawable.over)
+        }
+    }
+    private fun reset(){
+        imageCategory.setImageResource(R.drawable.defaultt)
+        editHeight.text.clear()
+        editWeight.text.clear()
+        textBmi.setText("")
+        textCategory.setText("")
+    }
+}
